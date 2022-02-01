f84e017bcf534cf66a878d6941da6514f0587a8d
Ihor Ryabets <eronryabets@gmail.com>
2022-02-02 00:23:59 +0200

    test @Around

--- src/main/java/aop/NewLoggingAspect.java
+++ src/main/java/aop/NewLoggingAspect.java
@@ -1,5 +1,6 @@
 package aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,19 @@ import org.springframework.stereotype.Component;
 public class NewLoggingAspect {
 
     @Around("execution ( public String returnBook())")
-    public void aroundReturnBookLoggingAdvice(){
-        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку возвращают книгу");
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint)
+            throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: " +
+                "в библиотеку пытаются вернуть книгу");
+        long begin = System.currentTimeMillis();
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        long end = System.currentTimeMillis();
+
+        System.out.println("aroundReturnBookLoggingAdvice: " +
+                "в библиотеку успешно вернули книгу");
+        System.out.println("aroundReturnBookLoggingAdvice: метод returnBook() "+
+                "выполнил работу за " + (end-begin) + " милисекунд.");
+        return targetMethodResult;
     }
 
 }
--- src/main/java/aop/Test3.java
+++ src/main/java/aop/Test3.java
@@ -2,26 +2,18 @@ package aop;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.List;
-
 public class Test3 {
     public static void main(String[] args) {
+        System.out.println("Methods main starts");
         try (AnnotationConfigApplicationContext context =
                      new AnnotationConfigApplicationContext(MyConfig.class)) {
 
-            University university = context.getBean("university", University.class);
-            university.addStudents();
-            try{
-                List<Student> students = university.getStudents();
-                System.out.println(students);
-            }
-            catch (Exception e){
-                System.out.println("Было поймано исключение " + e);
-            }
-
-
+            UniLibrary library = context.getBean("uniLibrary", UniLibrary.class);
+            String bookName = library.returnBook();
+            System.out.println("В библиотеку вернули книгу " + bookName);
 
         }
+        System.out.println("Methods main ends");
 
     }
 }
