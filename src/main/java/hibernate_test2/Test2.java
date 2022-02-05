c92a047dd30e1598eb1da6916efaddb23015ed5a
Ihor Ryabets <eronryabets@gmail.com>
2022-02-05 19:02:05 +0200

    hibernate test

--- src/main/java/hibernate_test2/Test2.java
+++ src/main/java/hibernate_test2/Test2.java
@@ -1,32 +1,29 @@
-package hibernate_test;
+package hibernate_test2;
 
-import hibernate_test.entity.Employee;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test5 {
+public class Test2 {
     public static void main(String[] args) {
 
         try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory()) {
 
             Session session = factory.getCurrentSession();
             session.beginTransaction();
 
-//            Employee emp = session.get(Employee.class, 1);
-//            session.delete(emp);
-            session.createQuery("delete Employee " +
-                    "where name='Aleksandr'").executeUpdate();
+
 
             session.getTransaction().commit();
-
             System.out.println("Done!");
-
         }
-
 
 
 
